07f68a00c29ab4166af6d32ea8bed82d14df7abe
tonnac <tonnac35@gmail.com>
2020-09-09 00:48:57 +0900

    3.1 Slow URLChecker

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 var errRequestFailed = errors.New("Request Failed")
 
 func main() {
+	var results = make(map[string]string)
+
 	urls := []string{
 		"https://www.airbnb.com/",
 		"https://www.google.com/",
@@ -19,15 +21,25 @@ func main() {
 		"https://www.instagram.com/",
 		"https://academy.nomadcoders.co/",
 	}
+
 	for _, url := range urls {
-		hitURL(url)
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	for url, result := range results {
+		fmt.Println(url, result)
 	}
 }
 
 func hitURL(url string) error {
 	fmt.Println("Cheking:", url)
 	resp, err := http.Get(url)
-	if err == nil || resp.StatusCode >= 400 {
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
 		return errRequestFailed
 	}
 	return nil
