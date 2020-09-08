a5a12d4511250c8c171165eab5ff5129da6f235b
tonnac <tonnac35@gmail.com>
2020-09-08 17:13:00 +0900

    1.8 Pointers

--- main.go
+++ main.go
@@ -2,25 +2,9 @@ package main
 
 import "fmt"
 
-func canIDrink(age int) bool {
-
-	switch koreanAge := age + 2; {
-	case koreanAge < 18:
-		return false
-	case koreanAge == 18:
-		return true
-	case koreanAge > 50:
-		return true
-	default:
-		return false
-	}
-
-	// if koreanAge := age + 2; koreanAge < 18 {
-	// 	return false
-	// }
-	// return true
-}
-
 func main() {
-	fmt.Println(canIDrink(16))
+	a := 2
+	b := &a
+	*b = 30
+	fmt.Println(a, *b)
 }
