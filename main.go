0eea6a9ff4e3f31ebbe1dd21184336dee6eba352
tonnac <tonnac35@gmail.com>
2020-09-08 16:52:46 +0900

    1.7 Switch

--- main.go
+++ main.go
@@ -3,12 +3,24 @@ package main
 import "fmt"
 
 func canIDrink(age int) bool {
-	if koreanAge := age + 2; koreanAge < 18 {
+
+	switch koreanAge := age + 2; {
+	case koreanAge < 18:
+		return false
+	case koreanAge == 18:
+		return true
+	case koreanAge > 50:
+		return true
+	default:
 		return false
 	}
-	return true
+
+	// if koreanAge := age + 2; koreanAge < 18 {
+	// 	return false
+	// }
+	// return true
 }
 
 func main() {
-	fmt.Println(canIDrink(18))
+	fmt.Println(canIDrink(16))
 }
