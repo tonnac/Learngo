35e9ebbbafc37f74c6c13116add7dc02c2ac0fdf
tonnac <tonnac35@gmail.com>
2020-09-09 01:58:03 +0900

    3.6 URLChecker + Go Routines

--- main.go
+++ main.go
@@ -1,22 +1,47 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"time"
+	"net/http"
 )
 
+type result struct {
+	url    string
+	status string
+}
+
+var errRequestFailed = errors.New("Request Failed")
+
 func main() {
-	c := make(chan string)
-	people := [5]string{"asdasd", "zxczxc", "poipoi", "oiuoiu", "kljlkj"}
-	for _, person := range people {
-		go isSexy(person, c)
+
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
-	for i := 0; i < len(people); i++ {
+
+	var c = make(chan result)
+
+	for _, url := range urls {
+		go hitURL(url, c)
+	}
+	for i := 0; i < len(urls); i++ {
 		fmt.Println(<-c)
 	}
 }
 
-func isSexy(person string, c chan string) {
-	time.Sleep(time.Second * 3)
-	c <- person + " is sexy"
+func hitURL(url string, c chan<- result) {
+	fmt.Println("Cheking:", url)
+	resp, err := http.Get(url)
+	status := "OK"
+	if err != nil || resp.StatusCode >= 400 {
+		status = fmt.Sprintf("FAILED %d", resp.StatusCode)
+	}
+	c <- result{url: url, status: status}
 }
