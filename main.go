6a277020a228f4ef8aed213fb6abee3a5656ee75
tonnac <tonnac35@gmail.com>
2020-09-08 16:37:17 +0900

    1.4 Functions part Two

--- main.go
+++ main.go
@@ -5,19 +5,16 @@ import (
 	"strings"
 )
 
-func letAndUpper(name string) (int, string) {
-	return len(name), strings.ToUpper(name)
-}
-
-func repeatMe(words ...string) {
-	fmt.Println(words)
+//Naked
+func letAndUpper(name string) (length int, uppercase string) {
+	defer fmt.Println("I'm done")
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
 }
 
 func main() {
 	totalLength, upperName := letAndUpper("ewwls")
-	totalLength2, _ := letAndUpper("ewqwewqwls")
 	fmt.Println(totalLength, upperName)
-	fmt.Println(totalLength2)
 
-	repeatMe("qqqq", "wwww", "eeee", "rrrr", "ttttt", "rrrrr", "tttt")
 }
