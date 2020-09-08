d4f7c26d0ba37032dd363028c1887185a9e9c142
tonnac <tonnac35@gmail.com>
2020-09-09 02:10:14 +0900

    3.7 FAST URLChecker

--- main.go
+++ main.go
@@ -6,15 +6,18 @@ import (
 	"net/http"
 )
 
-type result struct {
+type requestResult struct {
 	url    string
 	status string
+}
+
+func (r requestResult) String() string {
+	return fmt.Sprintf("%s is %s", r.url, r.status)
 }
 
 var errRequestFailed = errors.New("Request Failed")
 
 func main() {
-
 	urls := []string{
 		"https://www.airbnb.com/",
 		"https://www.google.com/",
@@ -26,22 +29,31 @@ func main() {
 		"https://academy.nomadcoders.co/",
 	}
 
-	var c = make(chan result)
+	var c = make(chan requestResult)
 
 	for _, url := range urls {
 		go hitURL(url, c)
 	}
+	// for i := 0; i < len(urls); i++ {
+	// 	fmt.Println(<-c)
+	// }
+
+	var results = map[string]string{}
 	for i := 0; i < len(urls); i++ {
-		fmt.Println(<-c)
+		result := <-c
+		results[result.url] = result.status
+	}
+
+	for url, status := range results {
+		fmt.Println(url, status)
 	}
 }
 
-func hitURL(url string, c chan<- result) {
-	fmt.Println("Cheking:", url)
+func hitURL(url string, c chan<- requestResult) {
 	resp, err := http.Get(url)
 	status := "OK"
 	if err != nil || resp.StatusCode >= 400 {
 		status = fmt.Sprintf("FAILED %d", resp.StatusCode)
 	}
-	c <- result{url: url, status: status}
+	c <- requestResult{url: url, status: status}
 }
