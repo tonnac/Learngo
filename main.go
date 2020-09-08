fcafccddbcb39513c8e145cc212d8168393e671e
tonnac <tonnac35@gmail.com>
2020-09-08 16:30:33 +0900

    1.3 Fucntions part One

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func letAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
 
 func main() {
-	name := true
-	fmt.Println(name)
+	totalLength, upperName := letAndUpper("ewwls")
+	totalLength2, _ := letAndUpper("ewqwewqwls")
+	fmt.Println(totalLength, upperName)
+	fmt.Println(totalLength2)
+
+	repeatMe("qqqq", "wwww", "eeee", "rrrr", "ttttt", "rrrrr", "tttt")
 }
