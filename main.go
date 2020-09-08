72a672f8cd74ce03ff22954e3f322c136e4712b3
tonnac <tonnac35@gmail.com>
2020-09-08 17:17:45 +0900

    1.9 Arrays and Slices

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	a := 2
-	b := &a
-	*b = 30
-	fmt.Println(a, *b)
+	names := []string{"qweq", "adasd", "dsfdsfd"}
+	names0 := append(names, "ekrjer")
+	names1 := append(names0, "pwerewr")
+	names2 := append(names1, "mmkwe")
+	fmt.Println(names)
+	fmt.Println(names0)
+	fmt.Println(names1)
+	fmt.Println(names2)
 }
