f98056e5ca7611c40d761d2ccd375327555ec541
tonnac <tonnac35@gmail.com>
2020-09-08 17:33:14 +0900

    1.11 Structs

--- main.go
+++ main.go
@@ -2,24 +2,14 @@ package main
 
 import "fmt"
 
+type person struct {
+	name    string
+	age     int
+	favFood []string
+}
+
 func main() {
-	// lkes := map[string]string{"name": "nico", "age": "12"}
-	kfd := [5]map[string]string{
-		{"name": "qweqwe", "age": "32"},
-		{"name0": "asdasd", "age0": "42"},
-		{"name1": "zxczxc", "age1": "52"},
-		{"name2": "rtyrty", "age2": "62"},
-		{"name3": "fghfgh", "age3": "72"}}
-	// for _, value := range lkes {
-	// 	fmt.Println(value)
-	// }
-
-	for _, map0 := range kfd {
-		for key, value := range map0 {
-			fmt.Println(key, value)
-		}
-	}
-
-	// fmt.Println(kfd)
-	// fmt.Println(lkes)
+	favFood := []string{"Kimchi", "Ramen"}
+	fsdf := person{name: "dddd", age: 18, favFood: favFood}
+	fmt.Println(fsdf.name)
 }
