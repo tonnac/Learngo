8b6861149110dd0a06ae12f058a8135d3e4d7d2d
tonnac <tonnac35@gmail.com>
2020-09-09 00:40:12 +0900

    3.0 hitURL

--- main.go
+++ main.go
@@ -1,22 +1,34 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/tonnac/learngo/mydict"
+	"net/http"
 )
 
-func main() {
-	dictionary := mydict.Dictionary{}
-	baseWord := "hello"
-	dictionary.Add(baseWord, "first")
-	err := dictionary.Update(baseWord, "Second")
-	if err != nil {
-		fmt.Println(err)
-	}
-	word, _ := dictionary.Search(baseWord)
-	fmt.Println(word)
+var errRequestFailed = errors.New("Request Failed")
 
-	dictionary.Delete(baseWord)
-	fmt.Println(dictionary)
+func main() {
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+	for _, url := range urls {
+		hitURL(url)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("Cheking:", url)
+	resp, err := http.Get(url)
+	if err == nil || resp.StatusCode >= 400 {
+		return errRequestFailed
+	}
+	return nil
 }
