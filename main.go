edad392df76fe01d2a8806779b33d6a7ef73c421
tonnac <tonnac35@gmail.com>
2020-09-09 17:01:47 +0900

    4.2 extractJob part One

--- main.go
+++ main.go
@@ -8,6 +8,14 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+type extractedJob struct {
+	id       string
+	title    string
+	location string
+	salary   string
+	summary  string
+}
+
 var baseURL string = "https://kr.indeed.com/jobs?q=python"
 
 func main() {
@@ -21,7 +29,23 @@ func main() {
 func getPage(page int) {
 	// strconv.Itoa(page * 50)
 	pageURL := fmt.Sprintf("%s&start=%d", baseURL, page*10)
-	fmt.Println("Requesting ", pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searchCards := doc.Find(".jobsearch-SerpJobCard")
+
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		id, _ := card.Attr("data-jk")
+		fmt.Println(id)
+		title := card.Find(".title>a").Text()
+		location := card.Find(".sjcl").Text()
+		fmt.Println(id, title, location)
+	})
 }
 
 func getPages() int {
@@ -51,4 +75,8 @@ func checkCode(res *http.Response) {
 	if res.StatusCode != 200 {
 		log.Fatalln("Request failed with Status: ", res.StatusCode)
 	}
+}
+
+func cleanString(str string) string {
+
 }
